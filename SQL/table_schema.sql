c4d14262bf5c4fef8ebb5a80e5dd248f8c5f125d
sydneysteele03 <126841347+sydneysteele03@users.noreply.github.com>
2023-08-22 20:32:38 -0700

    table schema sql

--- SQL/table_schema.sql
+++ SQL/table_schema.sql
@@ -16,3 +16,21 @@ CREATE TABLE wine_train (
 );
 
 SELECT * FROM wine_train;
+
+CREATE TABLE wine_test (
+	id INT, 
+	fixed_acidity FLOAT, 
+	volatile_acidity FLOAT, 
+	citric_acid FLOAT,
+	residual_sugar FLOAT,
+	chlorides FLOAT,
+	free_SO2 FLOAT, 
+	total_SO2 FLOAT, 
+	density FLOAT, 
+	pH FLOAT, 
+	sulphates FLOAT, 
+	alcohol FLOAT, 
+	PRIMARY KEY (id)
+);
+
+SELECT * FROM wine_test;
